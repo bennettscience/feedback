b930662115167d352a447f9b8c9fede1e8b8b3af
bennettscience <brian.bennett2@gmail.com>
2024-02-18 15:22:41 -0500

    Add active bool to standards

--- feedbook/models.py
+++ feedbook/models.py
@@ -39,7 +39,8 @@ class Standard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     description = db.Column(db.String(1000))
-        
+    active = db.Column(db.Boolean, default=True)
+            
     attempts = db.relationship(
         "StandardAttempt",
         backref=backref("standard", single_parent=True),
