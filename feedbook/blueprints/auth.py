18d1dfa01e7abe3042ee2878da529f041c165b72
bennettscience <brian.bennett2@gmail.com>
2024-11-12 21:25:38 -0500

    Update login flow
    Redirect the user back to the login page on a wrong input.

--- feedbook/blueprints/auth.py
+++ feedbook/blueprints/auth.py
@@ -5,39 +5,41 @@ from webargs import fields
 from webargs.flaskparser import parser
 
 from feedbook.blueprints import home
-from feedbook.extensions import db 
+from feedbook.extensions import db
 from feedbook.models import User
 
-bp = Blueprint('auth', __name__)
+bp = Blueprint("auth", __name__)
+
 
 @bp.get("/login")
 def get_login():
     if current_user.is_authenticated:
         return redirect(url_for(home.index))
 
-    return render_template(
-        "shared/forms/login.html"
-    )
-    
-    
+    return render_template("shared/forms/login.html")
+
+
 @bp.post("/login")
 def login():
-    args = parser.parse({
-        "email": fields.Str(),
-        "password": fields.Str(),
-        "remember_me": fields.Bool(load_default=False)
-    }, location="form")
+    args = parser.parse(
+        {
+            "email": fields.Str(),
+            "password": fields.Str(),
+            "remember_me": fields.Bool(load_default=False),
+        },
+        location="form",
+    )
 
-    user = User.query.filter(User.email == args['email']).first()
-    if user is None or not user.check_password(args['password']):
+    user = User.query.filter(User.email == args["email"]).first()
+    if user is None or not user.check_password(args["password"]):
         return make_response(
-            trigger={"showToast": "Username or password incorrect"}
+            redirect="/login",
+            trigger={"showToast": "Username or password incorrect"},
         )
 
-    login_user(user, remember=args['remember_me'])
-    return make_response(
-        redirect=url_for('home.index')
-    )
+    login_user(user, remember=args["remember_me"])
+    return make_response(redirect=url_for("home.index"))
+
 
 @bp.get("/register")
 def get_register():
@@ -45,6 +47,7 @@ def get_register():
     # return render_template(
     #     "shared/forms/register.html"
     # )
+
 
 @bp.post("/register")
 def register():
@@ -66,14 +69,13 @@ def register():
     # if user:
     #     print(f"{args['email']} already exists")
     #     return render_template(
-    #         "shared/partials/register-form.html", 
+    #         "shared/partials/register-form.html",
     #         error="That email is unavailable"
     #     )
-        
 
     # user = User(
-    #     email=args['email'], 
-    #     last_name=args['last_name'], 
+    #     email=args['email'],
+    #     last_name=args['last_name'],
     #     first_name=args['first_name'],
     #     usertype_id=2,
     # )
@@ -81,12 +83,13 @@ def register():
     # db.session.add(user)
     # db.session.commit()
     # login_user(user)
-    
+
     # return make_response(
     #     redirect=url_for('home.index')
     #     )
 
-@bp.get('/logout')
+
+@bp.get("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('home.index'))
+    return redirect(url_for("home.index"))
