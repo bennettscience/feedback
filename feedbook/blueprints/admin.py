44b97a7ad9eb11c104632102772dcbbc0d1c0193
bennettscience <brian.bennett2@gmail.com>
2024-12-14 22:29:53 -0500

    WIP course compare chart
    First pass at a chart comparing progress for each standard between the
    three classes.

--- feedbook/blueprints/admin.py
+++ feedbook/blueprints/admin.py
@@ -3,7 +3,7 @@ from flask_login import current_user, login_required
 from htmx_flask import make_response
 
 from feedbook.extensions import db
-from feedbook.models import Standard, User
+from feedbook.models import Course, Standard, User
 from feedbook.static.icons import *
 from feedbook.wrappers import restricted
 
@@ -18,5 +18,20 @@ def index():
     Load the main admin panel. Reload the sidebar because
     this extends the main layout right now.
     """
+    data = []
+    # Loop all the courses
+    courses = Course.query.all()
+    # For each course, build an array of all of the standards
+    for course in courses:
+        print(course.name)
+        standard_results = []
+        standards = course.standards.all()
+        for standard in standards:
+            print("{}, {}".format(course.name, standard.course_average(course.id)))
+            standard_results.append(
+                {"name": standard.name, "avg": standard.course_average(course.id)}
+            )
+        data.append({"course": course.name, "results": standard_results})
+
     icons = {"home": home, "add": add, "admin": admin, "logout": logout}
-    return render_template("admin/index.html", icons=icons)
+    return render_template("admin/index.html", icons=icons, status=data)
